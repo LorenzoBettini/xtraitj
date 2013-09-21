152c90dc6994d1c7a4b2fb5d7009223b1ca581f5
Lorenzo Bettini <bettini@dsi.unifi.it>
2013-09-21 19:57:25 +0200

    removed xtend-gen directories

--- xtraitj.tests/xtend-gen/xtraitj/input/tests/MyTestInterface.java
+++
@@ -1,8 +0,0 @@
-package xtraitj.input.tests;
-
-import java.util.List;
-
-@SuppressWarnings("all")
-public interface MyTestInterface {
-  public abstract int m(final List<String> l);
-}
--- xtraitj.tests/xtend-gen/xtraitj/input/tests/MyTestInterface2.java
+++
@@ -1,8 +0,0 @@
-package xtraitj.input.tests;
-
-import java.util.List;
-
-@SuppressWarnings("all")
-public interface MyTestInterface2 {
-  public abstract List<Integer> n(final int i);
-}
