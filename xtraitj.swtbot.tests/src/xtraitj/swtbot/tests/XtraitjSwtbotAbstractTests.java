32cf88205e343f333a80b30564e05dc8c33a8ec9
Lorenzo Bettini <bettini@dsi.unifi.it>
2015-04-29 19:41:06 +0200

    fix SWTBot test
    Change-Id: I42f1c7e3050163beb2fc9eefd87b80eb5849dfbe

--- xtraitj.swtbot.tests/src/xtraitj/swtbot/tests/XtraitjSwtbotAbstractTests.java
+++ xtraitj.swtbot.tests/src/xtraitj/swtbot/tests/XtraitjSwtbotAbstractTests.java
@@ -4,6 +4,7 @@ import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
 import static org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil.cleanWorkspace;
 import static org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil.root;
 import static org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil.waitForAutoBuild;
+import static org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil.cleanBuild;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -223,7 +224,11 @@ public class XtraitjSwtbotAbstractTests {
 		// creation of a project might require some time
 		bot.waitUntil(shellCloses(shell), SWTBotPreferences.TIMEOUT);
 		assertTrue("Project doesn't exist", isProjectCreated(mainProjectId));
-		
+
+		// this will also create xtraitj-gen folder
+		waitForAutoBuild();
+		// a clean build is required to simulate what happens in the Workbench
+		cleanBuild();
 		waitForAutoBuild();
 		assertErrorsInProject(0);
 	}
